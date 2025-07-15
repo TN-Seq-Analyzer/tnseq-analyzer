9ed4f777d1620beaff213e518ef2f5e8ec25ce81
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-07-15 01:05:22 -0300

    chore: initial project setup with Electron, Vite, React, Tailwind and TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+    return (
+        <>
+            <div className='w-full h-screen grid place-content-center'>
+                <h2 className="text-2x text-gray-900">Hello World!!!!!!!!!!!!</h2>
+            </div>
+        </>
+    )
+}
+
+export default App
