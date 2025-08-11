212a3108fa9c8d1caf73604279065871127ffffe
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-08-11 01:00:22 -0300

    feat: creates transposon sequence input component and organism id in addition to refactoring the input logic of choosing file

--- frontend/src/components/process-sample/ChooseFile.tsx
+++ frontend/src/components/process-sample/ChooseFile.tsx
@@ -6,9 +6,16 @@ import { Label } from "../ui/label";
 interface ChooseFileProps {
   fileName?: string;
   defaultFileName?: string;
+  disabled?: boolean;
+  handleOpen?: () => Promise<void>;
 }
 
-function ChooseFile({ fileName, defaultFileName }: ChooseFileProps) {
+function ChooseFile({
+  fileName,
+  defaultFileName,
+  disabled,
+  handleOpen,
+}: ChooseFileProps) {
   const fileNameToDisplay = fileName ? fileName : defaultFileName;
   const { t } = useTranslation("translation", {
     keyPrefix: "content.processSample",
@@ -20,11 +27,14 @@ function ChooseFile({ fileName, defaultFileName }: ChooseFileProps) {
       </Label>
       <div className="flex flex-1 gap-1">
         <Input
-          disabled
-          className="font-inter bg-bgInputFile text-textPrimary h-[30px] w-[60%] cursor-pointer rounded-sm border-0 text-[10px] font-bold select-none md:text-[10px]"
+          disabled={disabled}
+          className={`font-inter bg-bgInputFile ${!fileName ? "text-textPrimary" : "text-green-500"} h-[30px] w-[60%] rounded-sm border-0 text-[10px] font-bold select-none md:text-[10px]`}
           value={t(`file.${fileNameToDisplay}`)}
         />
-        <Button className="font-inter h-[30px] cursor-pointer rounded-sm text-xs font-normal select-none">
+        <Button
+          onClick={handleOpen}
+          className="font-inter h-[30px] cursor-pointer rounded-sm text-xs font-normal select-none"
+        >
           {t("button.upload")}
         </Button>
       </div>
--- frontend/src/components/process-sample/InputAdvanced.tsx
+++ frontend/src/components/process-sample/InputAdvanced.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 
@@ -8,12 +9,17 @@ function InputAdvanced({
   label: string;
   className?: string;
 }) {
+  const [value, setValue] = useState("0");
+
   return (
     <div className={`flex flex-col gap-1 ${className}`}>
       <Label className="font-poppins text-[10px] font-bold text-[var(--text-primaryGray)]">
         {label}
       </Label>
       <Input
+        min={0}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
         type="number"
         className="h-[28px] w-full border-[1px] border-gray-200 bg-white"
       />
