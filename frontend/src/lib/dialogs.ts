490407012f384644d73f15da66fb460f5c2988c1
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-08-11 01:45:30 -0300

    refactor: extends the extensions of the fastq and gff input files

--- frontend/src/lib/dialogs.ts
+++ frontend/src/lib/dialogs.ts
@@ -4,7 +4,7 @@ import fs from "fs";
 export async function handleOpenFileDialogFastq() {
   const { canceled, filePaths } = await dialog.showOpenDialog({
     properties: ["openFile"],
-    filters: [{ name: "Fastq", extensions: ["fastq"] }],
+    filters: [{ name: "Fastq", extensions: ["fastq", "fastq.gz"] }],
   });
 
   if (canceled) return null;
@@ -18,7 +18,7 @@ export async function handleOpenFileDialogFastq() {
 export async function handleOpenFileDialogGff() {
   const { canceled, filePaths } = await dialog.showOpenDialog({
     properties: ["openFile"],
-    filters: [{ name: "Gff", extensions: ["gff"] }],
+    filters: [{ name: "Gff", extensions: ["gff", "gff3"] }],
   });
 
   if (canceled) return null;
