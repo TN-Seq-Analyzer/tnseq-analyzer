00e257c5ecec96b7512ff2feda2f098476797bb7
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-08-11 03:50:08 -0300

    refactor: improves efficiency, restructures code and adds directory selection for analysis

--- frontend/src/lib/dialogs.ts
+++ frontend/src/lib/dialogs.ts
@@ -1,43 +1,58 @@
 import { dialog } from "electron";
 import fs from "fs";
 
-export async function handleOpenFileDialogFastq() {
-  const { canceled, filePaths } = await dialog.showOpenDialog({
-    properties: ["openFile"],
-    filters: [{ name: "Fastq", extensions: ["fastq", "fastq.gz"] }],
-  });
+export async function openDialog(
+  type: "fastq" | "gff" | "fasta" | "directory",
+) {
+  let properties: Array<"openFile" | "openDirectory"> = ["openFile"];
+  let filters: Array<{ name: string; extensions: string[] }> = [];
+  let readContent = true;
 
-  if (canceled) return null;
+  switch (type) {
+    case "fastq":
+      filters = [{ name: "Fastq", extensions: ["fastq", "fastq.gz"] }];
+      break;
+    case "gff":
+      filters = [{ name: "Gff", extensions: ["gff", "gff3"] }];
+      break;
+    case "fasta":
+      filters = [{ name: "Fasta", extensions: ["fasta"] }];
+      readContent = false;
+      break;
+    case "directory":
+      properties = ["openDirectory"];
+      filters = [];
+      readContent = false;
+      break;
+    default:
+      throw new Error("Tipo não suportado");
+  }
 
-  const filePath = filePaths[0];
-  const fileContent = fs.readFileSync(filePath, "utf8");
+  let dialogOptions: any = { properties };
+  if (filters.length > 0) {
+    dialogOptions.filters = filters;
+  }
 
-  return { filePath, fileContent };
-}
-
-export async function handleOpenFileDialogGff() {
-  const { canceled, filePaths } = await dialog.showOpenDialog({
-    properties: ["openFile"],
-    filters: [{ name: "Gff", extensions: ["gff", "gff3"] }],
-  });
-
-  if (canceled) return null;
-
-  const filePath = filePaths[0];
-  const fileContent = fs.readFileSync(filePath, "utf8");
-
-  return { filePath, fileContent };
-}
-
-export async function handleOpenFileDialogFasta() {
-  const { canceled, filePaths } = await dialog.showOpenDialog({
-    properties: ["openFile"],
-    filters: [{ name: "Fasta", extensions: ["fasta"] }],
-  });
-
-  if (canceled) return null;
-
+  const { canceled, filePaths } = await dialog.showOpenDialog(dialogOptions);
+  if (canceled || !filePaths.length) return null;
   const filePath = filePaths[0];
 
+  if (readContent) {
+    const fileContent = fs.readFileSync(filePath, "utf8");
+    return { filePath, fileContent };
+  }
   return { filePath };
+}
+
+export async function handleOpenFileDialogFastq() {
+  return openDialog("fastq");
+}
+export async function handleOpenFileDialogGff() {
+  return openDialog("gff");
+}
+export async function handleOpenFileDialogFasta() {
+  return openDialog("fasta");
+}
+export async function handleOpenFileDialogDirectory() {
+  return openDialog("directory");
 }
