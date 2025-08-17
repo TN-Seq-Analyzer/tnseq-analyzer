3d61898aafaee7a8e06ee596a7ab7b4acef50aae
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-08-16 23:22:12 -0300

    fix: change file type to add advanced parameters

--- frontend/src/hooks/useFileHandler.ts
+++ frontend/src/hooks/useFileHandler.ts
@@ -1,13 +1,23 @@
 import { useEffect, useState } from "react";
+import { FileData } from "../types/index";
 
 export function useFileHandler() {
-  const [files, setFiles] = useState({
+  const [files, setFiles] = useState<FileData>({
     fastq: { name: null, content: null },
     fasta: { name: null, content: null },
     gff: { name: null, content: null },
     directory: { directory: null },
     transpFile: "",
     idFile: "",
+    advancedParams: {
+      minimumReadLength: "0",
+      maximumReadLength: "0",
+      trimmingQuality: "0",
+      minimumMapingQuality: "0",
+      numberOfThreadsForAnalysis: "0",
+      minConfidenceThreshold: "0",
+      maxNonEssentialGenes: "0",
+    },
   });
 
   useEffect(() => {
@@ -26,7 +36,7 @@ export function useFileHandler() {
   const [transpFile, setTranspFile] = useState("");
   const [idFile, setIdFile] = useState("");
 
-  const persistFiles = (updatedFiles: typeof files) => {
+  const persistFiles = (updatedFiles: FileData) => {
     if (window.electronFile?.setFiles) {
       window.electronFile.setFiles(updatedFiles);
     }
@@ -34,7 +44,7 @@ export function useFileHandler() {
 
   const handleOpenFile = async (
     type: "fastq" | "gff" | "fasta",
-    field: keyof typeof files,
+    field: keyof FileData,
   ) => {
     let dialogFn;
     switch (type) {
@@ -66,16 +76,13 @@ export function useFileHandler() {
     }
   };
 
-  const handleOpenFastq = (field: keyof typeof files) =>
+  const handleOpenFastq = (field: keyof FileData) =>
     handleOpenFile("fastq", field);
-  const handleOpenGff = (field: keyof typeof files) =>
-    handleOpenFile("gff", field);
-  const handleOpenFasta = (field: keyof typeof files) =>
+  const handleOpenGff = (field: keyof FileData) => handleOpenFile("gff", field);
+  const handleOpenFasta = (field: keyof FileData) =>
     handleOpenFile("fasta", field);
 
-  const handleOpenDirectory = async (
-    field: keyof typeof files,
-  ): Promise<void> => {
+  const handleOpenDirectory = async (field: keyof FileData): Promise<void> => {
     const directory = await window.electronFile.openFileDialogDirectory();
     if (directory) {
       const { filePath } = directory;
