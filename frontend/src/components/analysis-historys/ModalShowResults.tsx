2fd13fb75114fec4d86ed288b0b6ffd84100127d
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-09-24 22:07:08 -0300

    feat: add internationalization to page analysis history

--- frontend/src/components/analysis-historys/ModalShowResults.tsx
+++ frontend/src/components/analysis-historys/ModalShowResults.tsx
@@ -1,8 +1,9 @@
 import { AnalysisRecord } from "@/utils/analysisHistoryFileUtils";
 import { formatDate } from "@/utils/formatDate";
 import { Button, Modal } from "antd";
+import { useTranslation } from "react-i18next";
 
-interface ModalShowDetailsProps {
+interface ModalShowResultsProps {
   resultsModalVisible: boolean;
   setResultsModalVisible: (visible: boolean) => void;
   selectedAnalysis: AnalysisRecord | null;
@@ -12,38 +13,44 @@ function ModalShowResults({
   resultsModalVisible,
   setResultsModalVisible,
   selectedAnalysis,
-}: ModalShowDetailsProps) {
+}: ModalShowResultsProps) {
+  const { t } = useTranslation("translation", {
+    keyPrefix: "content.analysisHistory.modal.results",
+  });
+
   return (
     <Modal
-      title="Resultados da Análise"
+      title={t("title")}
       open={resultsModalVisible}
       onCancel={() => setResultsModalVisible(false)}
       footer={[
         <Button key="close" onClick={() => setResultsModalVisible(false)}>
-          Fechar
+          {t("close")}
         </Button>,
       ]}
-      width="80%"
-      centered
     >
       {selectedAnalysis && (
-        <div className="font-inter text-xs">
+        <div className="flex flex-col gap-3">
           <p>
-            <strong>Projeto:</strong> {selectedAnalysis.projectName}
+            <span className="font-bold">{t("projectName")}: </span>
+            {selectedAnalysis.projectName}
           </p>
           <p>
-            <strong>Data:</strong> {formatDate(selectedAnalysis.date)}
+            <span className="font-bold">{t("date")}: </span>
+            {selectedAnalysis.date}
           </p>
           <p>
             <strong>Status:</strong> {selectedAnalysis.status}
           </p>
-          {selectedAnalysis.results && (
+          {selectedAnalysis.results ? (
             <div className="mt-3 max-h-[400px] overflow-y-auto rounded-md bg-gray-50 p-3">
               <h4 className="mb-2 font-semibold">Resultados:</h4>
               <pre className="text-xs whitespace-pre-wrap">
                 {JSON.stringify(selectedAnalysis.results, null, 2)}
               </pre>
             </div>
+          ) : (
+            <p className="text-sm text-gray-500">{t("noSummary")}</p>
           )}
         </div>
       )}
